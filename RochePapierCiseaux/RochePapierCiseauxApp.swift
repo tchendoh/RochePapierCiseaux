ddac9ff9347663b37d9c204b7411f65cb17f61a7
Eric Chandonnet <tchendoh@gmail.com>
2025-04-25 15:22:35 -0400

    Initial Commit

---
+++ RochePapierCiseaux/RochePapierCiseauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RochePapierCiseauxApp.swift
+//  RochePapierCiseaux
+//
+//  Created by Eric Chandonnet on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct RochePapierCiseauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RochePapierCiseauxUITests/RochePapierCiseauxUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  RochePapierCiseauxUITestsLaunchTests.swift
+//  RochePapierCiseauxUITests
+//
+//  Created by Eric Chandonnet on 2025-04-25.
+//
+
+import XCTest
+
+final class RochePapierCiseauxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
